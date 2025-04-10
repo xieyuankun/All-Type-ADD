a86a7c772e6e4869cf2b35c180654ab324c8e5da
Yuankun Xie <51701794+xieyuankun@users.noreply.github.com>
2025-04-10 23:26:53 +0800

    Update T-SNE.sh

--- script/T-SNE.sh
+++ script/T-SNE.sh
@@ -1,5 +1,1 @@
-# python T-SNE.py --gpu 7 --model_path /data3/xyk/crosstype/tencent/ckpt_wpt_final/cotrain_wpt_46-w2v2aasist2
-# python T-SNE.py --gpu 7 --model_path /data3/xyk/crosstype/tencent/ckpt_pt/cotrain_pt_10t-w2v2aasist
-# python T-SNE.py --gpu 5 --model_path /data3/xyk/crosstype/tencent/ckpt_cotrain/cotrain_ft-w2v2aasist &
-# python T-SNE_wpt.py --gpu 6 --model_path /data3/xyk/crosstype/tencent/ckpt_wpt_final/cotrain_wpt_46-w2v2aasist2 & 
-python T-SNE_pt.py --gpu 7 --model_path /data3/xyk/crosstype/tencent/ckpt_pt/cotrain_pt_10t-w2v2aasist 
+python T-SNE.py --gpu 7 --model_path ./ckpt_best/cotrain_wpt_xlsraasist
